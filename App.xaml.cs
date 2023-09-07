578603f87f8c9a9f6d4de70daa7c1b3fa868b8b5
Oli <oliverposa@outlook.com>
2023-09-07 08:11:58 +0800

    PUSSHHHH

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace PortaJel_Blazor;
+﻿using Microsoft.Maui.Controls;
+
+namespace PortaJel_Blazor;
 
 public partial class App : Application
 {
 	public App()
+<<<<<<< HEAD
 	{
 		InitializeComponent(); 
         MainPage = new MainPage();
+=======
+    {
+		InitializeComponent();
+        MainPage = new NavigationPage(new MainPage());
+>>>>>>> 9db61e6d4a4e0f65aa645f4f807a0f0e7a31b80f
     }
 }
