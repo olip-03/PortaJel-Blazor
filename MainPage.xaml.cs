4e3b07a88572d849eac9435d99326300a1b37613
Oli <oliverposa@outlook.com>
2024-01-18 22:39:24 +0800

    Updates to music player controller

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,8 +13,22 @@ public partial class MainPage : ContentPage
 	{
         InitializeComponent();
         //NavigationPage.SetHasNavigationBar(this, false);
-        
-		MauiProgram.webView = blazorWebView;
+
+        #if ANDROID
+        btn_navnar_home.HeightRequest = 30;
+        btn_navnar_home.WidthRequest = 30;
+
+        btn_navnar_search.HeightRequest = 30;
+        btn_navnar_search.WidthRequest = 30;
+
+        btn_navnar_library.HeightRequest = 30;
+        btn_navnar_library.WidthRequest = 30;
+
+        btn_navnar_favourites.HeightRequest = 30;
+        btn_navnar_favourites.WidthRequest = 30;
+#endif
+
+        MauiProgram.webView = blazorWebView;
     }
     protected override void OnHandlerChanged()
     {
@@ -22,10 +36,29 @@ public partial class MainPage : ContentPage
 		// Disabled overscroll 'stretch' effect that I fucking hate.
 		// CLEAR giveaway this app uses webview lolz
 
-		#if ANDROID
-				var blazorView = this.blazorWebView;
-				var platformView = (Android.Webkit.WebView)blazorView.Handler.PlatformView;
-				platformView.OverScrollMode = Android.Views.OverScrollMode.Never;
-		#endif
+        #if ANDROID
+				        var blazorView = this.blazorWebView;
+				        var platformView = (Android.Webkit.WebView)blazorView.Handler.PlatformView;
+				        platformView.OverScrollMode = Android.Views.OverScrollMode.Never;
+        #endif
     }
+
+    #region Interactions
+    private void btn_navnar_home_click(object sender, EventArgs e)
+    {
+        MauiProgram.mainLayout.NavigateHome();
+    }
+    private void btn_navnar_search_click(object sender, EventArgs e)
+    {
+        MauiProgram.mainLayout.NavigateSearch();
+    }
+    private void btn_navnar_library_click(object sender, EventArgs e)
+    {
+        MauiProgram.mainLayout.NavigateLibrary();
+    }
+    private void btn_navnar_favourite_click(object sender, EventArgs e)
+    {
+        MauiProgram.mainLayout.NavigateFavourites();
+    }
+    #endregion
 }
