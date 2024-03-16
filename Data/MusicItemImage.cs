6c18acf4737c10177e315dd307d3e4910b439384
Oli <oliverposa@outlook.com>
2024-03-16 16:50:17 +0800

    Blurhash implemented on AlbumView

--- Data/MusicItemImage.cs
+++ Data/MusicItemImage.cs
@@ -1,7 +1,9 @@
 ﻿using Blurhash;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,6 +46,52 @@ namespace PortaJel_Blazor.Data
                 return toReturn;
             }
             return source;
+        }
+        public string BlurhashToBase64(int width = 0, int height = 0)
+        {
+            // Assuming you have a method to decode the blurhash into a pixel array
+            Pixel[,] pixels = new Pixel[width, height];
+            Blurhash.Core.Decode(blurHash, pixels);
+
+            // Create a SkiaSharp bitmap
+            using (SKBitmap bitmap = new SKBitmap(width, height))
+            {
+                // Lock the pixels of the bitmap
+                using (var canvas = bitmap.PeekPixels())
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            var pixel = pixels[x, y];
+                            int scaledRed = (int)(pixel.Red * 255); scaledRed = Math.Clamp(scaledRed, 0, 255);
+                            int scaledGreen = (int)(pixel.Green * 255); scaledGreen = Math.Clamp(scaledGreen, 0, 255);
+                            int scaledBlue = (int)(pixel.Blue * 255); scaledBlue = Math.Clamp(scaledBlue, 0, 255);
+
+                            // Convert int to byte
+                            byte red = (byte)scaledRed;
+                            byte green = (byte)scaledGreen;
+                            byte blue = (byte)scaledBlue;
+
+                            // Create SKColor
+                            SKColor color = new SKColor(red, green, blue);
+                            bitmap.SetPixel(x, y, color);
+                            // Set the pixel color
+                        }
+                    }
+                }
+
+                // Convert the bitmap to a base64 string
+                using (var image = SKImage.FromBitmap(bitmap))
+                using (var data = image.Encode(SKEncodedImageFormat.Jpeg, 100))
+                using (var stream = new MemoryStream())
+                {
+                    data.SaveTo(stream);
+                    byte[] imageBytes = stream.ToArray();
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
         }
     }
 }
---
+++ wwwroot/js/imageloader.js
@@ -0,0 +1,13 @@
+﻿function InitImageLoader(itemId) {
+    var image = document.getElementById(itemId);
+
+    function loaded() {
+        image.style.opacity = "1"; // Make the full image visible
+    }
+
+    if (image.complete) {
+        loaded();
+    } else {
+        image.addEventListener("load", loaded);
+    }
+};
